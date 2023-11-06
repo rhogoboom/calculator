1e8062f170f42afc9ca9a1181e63f561b452d24b
Richard Hogoboom <rhogoboom@gmail.com>
2023-11-06 15:22:44 -0500

    feat: operate based on chosen operator, first and second number

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,11 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operator, firstNumber, secondNumber) {
+    return operator(firstNumber, secondNumber);
+}
