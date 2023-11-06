9ecf13cd4bf81ee783e53744879ea62d7e5881b9
Richard Hogoboom <rhogoboom@gmail.com>
2023-11-06 17:09:02 -0500

    feat: basic operations functions work on start

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
+
+const display = document.querySelector('.screen') 
+const digitButtons = [...document.querySelectorAll('button.digit')]
+const operationButtons = [...document.querySelectorAll('button.operation')].filter(btn => btn.id !== 'operate' && btn.id !== 'clear');
+const operateButton = document.querySelector('#operate');
+
+digitButtons.forEach(btn => btn.addEventListener('click', addDigitToDisplay));
+operationButtons.forEach(btn => btn.addEventListener('click', handleOperationClick));
+operateButton.addEventListener('click', handleOperateClick);
+
+
+let displayValue = display.textContent;
+
+
 function add (x, y) {
     return x + y;
 }
@@ -14,10 +32,61 @@ function divide(x, y) {
     return x / y;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+const operators = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+}
 
 function operate(operator, firstNumber, secondNumber) {
     return operator(firstNumber, secondNumber);
+}
+
+function addDigitToDisplay(e) {
+    const thisDigit = e.target.textContent;
+    const currentDisplayValue = getCurrentDisplayValue();
+    updateDisplayValue(currentDisplayValue + thisDigit);
+    renderDisplay(displayValue);
+}
+
+function updateDisplayValue(newValue) {
+    displayValue = newValue;
+    return;
+}
+
+function renderDisplay() {
+    display.textContent = displayValue
+}
+
+function getCurrentDisplayValue() {
+    return displayValue;
+    
+}
+
+
+
+
+
+
+
+function handleOperationClick(e) {
+    console.log(e.target.id);
+    firstNumber = getCurrentDisplayValue();
+    operator = operators[e.target.id];
+    
+    updateDisplayValue('');
+    renderDisplay();
+}
+
+function handleOperateClick(e) {
+    if (firstNumber !== null && operator !== null) {
+        secondNumber = getCurrentDisplayValue();
+        let result = operate(operator, firstNumber, secondNumber);
+        firstNumber = result;
+        operator = null;
+        secondNumber = null;
+        updateDisplayValue(result);
+        renderDisplay();
+    }
 }
